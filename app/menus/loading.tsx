2ca6709f05052e3adddd936bcaa649a88ce5634d
Tomomi Hori <oom0n0moo@gmail.com>
2026-08-05 22:17:46 +0900

    提案履歴にもローディング追加

--- app/menus/loading.tsx
+++ app/menus/loading.tsx
@@ -8,11 +8,10 @@ export default function Loading() {
       <div className="w-full max-w-[900px] flex flex-row gap-6 items-start justify-center mt-6">
         <div className="flex-1 bg-white rounded-3xl shadow-xl p-12 sm:p-16 border border-slate-100 w-full min-w-0 flex flex-col items-center justify-center">
           
-          {/* クルクル回るスピナー */}
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#53cbfb] mb-4"></div>
           
           <p className="text-gray-500 font-bold text-sm tracking-wider">
-            ごはん一覧を読み込んでいます...
+            ごはん登録を読み込んでいます...
           </p>
 
         </div>
