9a1557fb981894f6eb1aa8408f4e5cf89c514c6b
Tomomi Hori <oom0n0moo@gmail.com>
2026-08-18 22:36:16 +0900

    難易度Cレビューの要改善について修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,15 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ごはん？なんでもいい〜",
+  description: "家族の好きなメニューだけを登録して検索表示できるアプリです。これを使えば、なんでもいい～と言われてもOK！",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" suppressHydrationWarning>
-      <body className="bg-[#54C7F3] min-h-screen antialiased overflow-y-scroll">
+      <body className="bg-[#54C7F3] min-h-screen antialiased overflow-y-scroll overflow-x-hidden">
         {children}
       </body>
     </html>
